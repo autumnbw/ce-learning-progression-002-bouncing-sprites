3bda14a569b6bb8c13c10c9668e9d8257c51ed31
autumnbw <70179578+autumnbw@users.noreply.github.com>
2020-09-24 13:15:00 -0600

    Create microbit-program-1-2-1.js

---
+++ programs/microbit-program-1-2-1.js
@@ -0,0 +1,10 @@
+const MAXFLASH : number = 10
+for (let i = 0; i < MAXFLASH; i ++) {
+  if (i % 2 == 1) {       
+    basic.showIcon(IconNames.Heart)  
+    basic.clearScreen()
+} else {
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(100)
+    basic.clearScreen()
+}}
