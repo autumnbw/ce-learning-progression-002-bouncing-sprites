a5754213b91325341c4b026fcd671033770b80d9
autumnbw <70179578+autumnbw@users.noreply.github.com>
2020-09-24 18:15:18 -0600

    Create microbit-program-1-2-2.js

---
+++ programs/microbit-program-1-2-2.js
@@ -0,0 +1,10 @@
+
+basic.forever(function () {
+    for (let x = 0; x < 5; x++) 
+    for (let y = 0; y < 5; y++) { 
+            led.plot(x,y)
+            basic.pause(50)
+            basic.clearScreen()
+             x = x + 1
+    
+}})
